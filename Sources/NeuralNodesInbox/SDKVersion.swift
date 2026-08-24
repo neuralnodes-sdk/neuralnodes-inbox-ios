fe8122a6b0dcae11a82ca8ad8cdf9c0ddde4fc9f
neuralnodes-sdk <neural.nodes.ai@gmail.com>
2026-08-24 10:36:33 +0530

    Version 2.2.1

--- Sources/NeuralNodesInbox/SDKVersion.swift
+++ Sources/NeuralNodesInbox/SDKVersion.swift
@@ -2,7 +2,7 @@ import Foundation
 
 /// SDK Version Information
 public struct SDKVersion {
-    public static let version = "2.2.0"
+    public static let version = "2.2.1"
     
     public static let name = "NeuralNodesInbox-iOS"
     
